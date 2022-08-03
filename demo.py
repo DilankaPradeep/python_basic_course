ef6734dd45384ca06abb712c3e179fc9b2e794db
DilankaPradeep <dilanka13627@gmail.com>
2022-08-03 13:52:02 +0530

    python basic functions

---
+++ demo.py
@@ -0,0 +1,35 @@
+print("Dilanka with python")
+print(123)
+print(1.24)
+print('Dilanka with python')
+print(True)
+print(False)
+
+print('Dilanka \n with python')
+print('Name\t : Dilanka')
+print('Country\t : Sri Lanka')
+
+print("Dilanka said \"Good Morning\"")
+print('Dilanka \\n with python')
+
+a= 10
+b = 2
+
+print( a/ b)
+
+# modiulus
+print(a % b)
+
+
+# flow devision
+print(a // b)
+
+# power of
+print(a ** b)
+
+c = True
+d = False
+# XOR gate impl
+print(c^d)
+
+
